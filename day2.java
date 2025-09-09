5e83f6434b415839d7abc2b966dc3b90898e5d18
Midun86 <midund682@gmail.com>
2025-09-09 21:53:03 +0800

    day2.java

---
+++ day2.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.print("hay ");
+        System.out.printf("nama saya %s%n","Midun");
+        System.out.printf("umur,19 tahun");
+        
+    }
+    
+}
