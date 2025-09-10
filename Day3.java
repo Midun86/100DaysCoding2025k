c6ec3ed6fdf7b6bd422169afa710e3ff48fa54d6
Midun86 <midund682@gmail.com>
2025-09-10 19:32:51 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println(" Halo nama saya Midun ");
+        // Hay ini adalah  komentar //
+        
+        /* dan ini juga komentar ,tapi kalimat yang lebih panjang
+         dan bisa lebih dari satu baris */
+    }
+    
+}
