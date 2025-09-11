59bb4c3bd6f1ff012f51aa08de15a6ed8fc75f42
Midun86 <midund682@gmail.com>
2025-09-11 19:49:31 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama\t\t:Midun");
+        System.out.println("Umur\t\t:19 tahun");
+        System.out.println("Alamat\t\t:Desa Betteng\n");
+        System.out.println("Hobi\t\t:Main polli");
+        System.out.println("Istatus\t\t:Mahasiswa");
+        
+    }
+    
+}
