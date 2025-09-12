6b29e5180dd8467e6204fb9888baf126728d5bd5
Midun86 <midund682@gmail.com>
2025-09-12 21:03:15 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main(String[] args) {
+         byte umur = 20 ;
+        short beratbadanb = 45 ;
+        int tinggibadanc = 170 ;
+        long gajid = 10000L ;
+        
+        System.out.println("Midun") ;
+        System.out.println("umur") ;
+        System.out.println("19") ;
+        System.out.println("tahun") ;
+        
+    }
+    
+}
