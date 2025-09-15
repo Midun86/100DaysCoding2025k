6d4c505d6cd18c0c9b4a0e2449dd38130f98452b
Midun86 <midund682@gmail.com>
2025-09-15 23:12:07 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Midun";
+        String pesan = " aku menyayangimu ";
+        
+        System.out.println("Hay nam saya midun " + nama);
+        System.out.println("saya midun " + pesan);
+        
+    }
+    
+}
