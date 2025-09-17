e5fc064a9a9620d2ca0074e1f94ca77ddf46992c
Midun86 <midund682@gmail.com>
2025-09-17 21:24:45 +0800

    Day10.java

---
+++ Day. /Day10.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int umur = 19;
+        final int tahun_ini = 2025;
+        
+        System.out.println("umur saya "+ umur);
+        System.out.println("final "+ tahun_ini);
+        
+    }
+    
+}
