098b2ee1dc3e3e55f16c69c3e38ca6177cf7c964
Midun86 <midund682@gmail.com>
2025-09-18 21:57:58 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama Anda: ");
+        String nama = scanner.nextLine(); 
+        System.out.println(nama);
+        
+    }
+    
+}
