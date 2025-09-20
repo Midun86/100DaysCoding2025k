bc2c8145d50329dce5827a5eaebbe71a5f805535
Midun86 <midund682@gmail.com>
2025-09-20 20:42:16 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static void main(String[] args) {
+            String nama="midun";
+		           int umur=25;
+            double ipk=3.37;
+		           System.out.println(" nama :"+ nama);
+		           System.out.print("umur :"+ umur);
+		           System.out.printf("nama :%s ,umur :%d ,ipk :%f" , nama , umur , ipk);
+		 
+    }
+    
+}
