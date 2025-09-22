63d75e807b6dbe960249be82ab5b9436916d319d
Midun86 <midund682@gmail.com>
2025-09-22 22:37:06 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 7;
+        
+        int hasil = a + b;
+        System.out.println("penjumlahan + pengurangan = " + hasil); 
+        
+        int selisih = a - b;
+        System.out.println("penjumlahan - pengurangan = " + selisih);
+    }
+    
+}
