c8575d962ebdcd89e27855c5d71b278c5946ec99
Midun86 <midund682@gmail.com>
2025-09-23 22:24:21 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int y = 8;
+        int p = 1;
+        
+        int hasilperkalian= y*p;
+        System.out.println("perkalian + perkalian=" + hasilperkalian);
+        
+        int u = 8;
+        int k = 2;
+        int hasilpembagian = u/k;
+        System.out.println("pembagiab + pembagian=" + hasilperkalian);
+        
+    }
+    
+}
