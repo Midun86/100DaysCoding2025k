295b6c7174e18588f8155aa3de0b7a4f5c60594b
Midun86 <midund682@gmail.com>
2025-09-24 22:53:36 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        	    
+    //contoh modulus atau sisa bagi dalam operasi aritmatika
+    
+        
+        int angka1= 10;
+        int angka2= 3;
+        int hasil= angka1 % angka2;
+        System.out.println("Sisa pembagian " + angka1 + " dan " + angka2 + " adalah: " + hasil);
+    }
+    
+}
