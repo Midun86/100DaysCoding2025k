53b8199a9d8e380c8d0133645d78aec076582c9e
Midun86 <midund682@gmail.com>
2025-09-27 22:32:00 +0800

    day20.java

---
+++ day20.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int angkaPrimitif = 123;
+        
+    String angkaString = String.valueOf(angkaPrimitif);
+    System.out.println(angkaString); // Output: 123
+        
+    }
+    
+}
