a41ed24b77065ae373fbe23ca23f542e7f17bd1e
Midun86 <midund682@gmail.com>
2025-09-28 22:44:19 +0800

    Day21.java

--- Day21.java
+++ Day21.java
@@ -1,24 +1,22 @@
 public class Main {
     
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        
-        System.out.println("sebelum ditukar:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        
-        //1.simpan nila 'a' ke variabel sementara 'temp'
-        int temp =a;
-        //2.simapan nila 'b'ke 'a'
-        a=b;
-        //3.simpan nilai 'temp' ( nilai asli 'a' ) ke 'b' 
-        b=temp;
-        
-        System.out.println("\nsetelah ditukar:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Sebelum ditukar: a = " + a + ", b = " + b);
+
+        // Proses penukaran nilai
+        a = a + b; // a menjadi 15
+        b = a - b; // b menjadi 15 - 10 = 5
+        a = a - b; // a menjadi 15 - 5 = 10
+
+        System.out.println("Setelah ditukar: a = " + a + ", b = " + b);
     }
-    
 }
+        
+        
+        
+    
+    
+
