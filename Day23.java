757cc4e290369c40c8a78250acb707786b94c305
Midun86 <midund682@gmail.com>
2025-09-30 20:19:12 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        double panjang = 20;
+        double lebar = 5;
+        double luas = panjang * lebar;
+        System.out.println("luas persegi panjang adalah: " + luas);
+        
+    }
+    
+}
